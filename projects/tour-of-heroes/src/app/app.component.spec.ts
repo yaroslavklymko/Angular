865c463ad4a809f9e7d06fdfa93e44216b500bba
Iaroslav Klymko <iaroslavk@starstechnologies.com>
2018-11-19 16:11:49 -0500

    hero component created and tests updated

--- projects/tour-of-heroes/src/app/app.component.spec.ts
+++ projects/tour-of-heroes/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 describe('AppComponent', () => {
   const title = 'Tour of Heroes';
@@ -7,7 +8,8 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeroesComponent
       ],
     }).compileComponents();
   }));
@@ -24,10 +26,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual(title);
   });
 
+  /*
+  not valid anymore
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ' + title + '!');
   });
+  */
 });
