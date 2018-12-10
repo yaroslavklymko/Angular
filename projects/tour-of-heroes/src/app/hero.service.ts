ecbf07debadfe1c24aa7ac6156d75368583e68e6
Iaroslav Klymko <iaroslavk@starstechnologies.com>
2018-12-10 17:55:07 -0500

    http put: update hero

--- projects/tour-of-heroes/src/app/hero.service.ts
+++ projects/tour-of-heroes/src/app/hero.service.ts
@@ -35,6 +35,19 @@ export class HeroService {
     );
   }
 
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
   /**
    * Handle Http operation that failed.
    * Let the app continue.
